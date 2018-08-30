144232e3729dd5f433d72f66c66ecbb3ae802a98
zhoujun <zhoujun@knowbox.cn>
2018-08-30 14:46:27 +0800

    project init
    project init

---
+++ EffectiveObjectiveC/EffectiveObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EffectiveObjectiveC
+//
+//  Created by Jun Zhou on 2018/8/30.
+//  Copyright © 2018年 Jun Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EffectiveObjectiveC/EffectiveObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EffectiveObjectiveC
+//
+//  Created by Jun Zhou on 2018/8/30.
+//  Copyright © 2018年 Jun Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
