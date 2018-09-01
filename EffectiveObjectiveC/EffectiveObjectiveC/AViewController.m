331b8cfa3318c52eeec8cb94b57a22b3060b728a
zhoujun <zhoujun@knowbox.cn>
2018-09-01 14:00:43 +0800

    NStimer category

--- EffectiveObjectiveC/EffectiveObjectiveC/AViewController.m
+++ EffectiveObjectiveC/EffectiveObjectiveC/AViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "AViewController.h"
+#import "NSTimer+Block.h"
 
 @interface AViewController ()
+
+@property (nonatomic, strong) NSTimer *timer1;
+@property (nonatomic, strong) NSTimer *timer2;
 
 @end
 
@@ -24,6 +28,24 @@
     NSLog(@"A viewDidLoad");
     
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    __weak typeof(self) weakSelf = self;
+//    self.timer1 = [NSTimer timerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+//        [weakSelf print1];
+//    }];
+//    [[NSRunLoop currentRunLoop] addTimer:self.timer1 forMode:NSDefaultRunLoopMode];
+//
+//    self.timer2 = [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+//        [weakSelf print2];
+//    }];
+    
+    self.timer1 = [NSTimer b_timerWithTimeInterval:1 repeats:YES inRunLoop:[NSRunLoop currentRunLoop] forMode:(NSDefaultRunLoopMode) block:^(NSTimer *timer) {
+        [weakSelf print1];
+    }];
+    
+    self.timer2 = [NSTimer b_scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer *timer) {
+        [weakSelf print2];
+    }];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -44,6 +66,20 @@
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     NSLog(@"A viewWillDisappear");
+}
+
+- (void)print1 {
+    NSLog(@"timer1 log");
+}
+
+- (void)print2 {
+    NSLog(@"timer2 log");
+}
+
+- (void)dealloc {
+    [self.timer1 invalidate];
+    [self.timer2 invalidate];
+    NSLog(@"%@ delloc", self);
 }
 
 
---
+++ EffectiveObjectiveC/EffectiveObjectiveC/NSTimer+Block.h
@@ -0,0 +1,20 @@
+//
+//  NSTimer+Block.h
+//  EffectiveObjectiveC
+//
+//  Created by Jun Zhou on 2018/9/1.
+//  Copyright © 2018年 Jun Zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSTimer (Block)
+
+/** 带有block回调的timer timer需加入runloop */
++ (NSTimer *)b_timerWithTimeInterval:(NSTimeInterval)interval repeats:(BOOL)repeats inRunLoop:(NSRunLoop *)runloop forMode:(NSRunLoopMode)mode block:(void (^)(NSTimer *timer))block;
+
+/** 带有block回调的timer timer自动加入当前runloop */
++ (NSTimer *)b_scheduledTimerWithTimeInterval:(NSTimeInterval)interval repeats:(BOOL)repeats block:(void (^)(NSTimer *timer))block;
+
+
+@end
